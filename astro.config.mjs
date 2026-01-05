9aba511890afc1731ebbb7104cc7269d9253b17c
stafast <stafast@outlook.com>
2026-01-05 09:57:14 +0100

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,26 +2,38 @@
 import {defineConfig} from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import icon from "astro-icon";
-import tailwind from '@astrojs/tailwind';
 import astroExpressiveCode from 'astro-expressive-code'
+import Sonda from 'sonda/astro';
+
+
+import tailwindcss from '@tailwindcss/vite';
 
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://stafast.net',
     trailingSlash: 'always',
+
     prefetch: {
         defaultStrategy: 'viewport'
     },
+
     integrations: [
         sitemap(),
         icon(),
+        Sonda(),
         astroExpressiveCode({
             themes: ['github-dark', 'github-light'],
             styleOverrides: {
                 codeFontSize: "0.9rem",
             },
         }),
-        tailwind({nesting: true})
-    ]
+    ],
+
+    vite: {
+        plugins: [tailwindcss()],
+        build: {
+            sourcemap: true
+        }
+    }
 });
--- src/data/constants.ts
+++ src/data/constants.ts
@@ -10,13 +10,16 @@ export interface Links {
 
 export const author: string = 'André Stafast';
 export const title: string = 'Software-Entwickler';
+export const defaultPageTitle: string = 'Stafast Studio – Websites, Webanwendungen & technische Beratung';
 export const defaultPageDescription: string = 'André Stafast, Software-Entwickler aus Neumünster. Spezialisiert auf Frontend, E-Commerce, Webentwicklung & Backend.'
 export const intro: string = 'Moin 👋, ich bin André Stafast, Entwickler aus Neumünster in Schleswig-Holstein. Hauptberuflich arbeite ich bei <a href="/#trinext">TriNext</a> als Entwickler und nebenbei bin ich als <a href="/#self-employed">Freelancer</a> aktiv.'
 
 export const navigation: NavigationItem[] = [
-    { title: 'Home', url: '/' },
-    { title: 'Blog', url: '/blog/' },
-    { title: 'Kontakt', url: '/kontakt/' }
+    { title: 'Lösungen', url: '/' },
+    { title: 'Leistungen', url: '/' },
+    { title: 'Kunden', url: '/' },
+    { title: 'Über mich', url: '/' },
+    { title: 'Projekt anfragen', url: '#kontakt' }
 ];
 
 export const links: Links[] = [
