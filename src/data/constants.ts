fc3b6166b7b7b716cc891439bb7fc7eabc765383
stafast <stafast@outlook.com>
2026-02-03 20:47:29 +0100

    Update

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -10,8 +10,8 @@ export interface Links {
 
 export const author: string = 'André Stafast';
 export const title: string = 'Software-Entwickler';
-export const defaultPageDescription: string = 'André Stafast, Software-Entwickler aus Neumünster. Spezialisiert auf Frontend, E-Commerce, Webentwicklung & Backend.'
-export const intro: string = 'Moin 👋, ich bin André Stafast, Entwickler aus Neumünster in Schleswig-Holstein. Hauptberuflich arbeite ich bei <a href="/#trinext">TriNext</a> als Entwickler und nebenbei bin ich als <a href="/#self-employed">Freelancer</a> aktiv.'
+export const defaultPageDescription: string = 'André Stafast, Software-Entwickler aus Kiel. Spezialisiert auf Frontend, E-Commerce, Webentwicklung & Backend.'
+export const intro: string = 'Moin 👋, ich bin André Stafast, Entwickler aus Kiel in Schleswig-Holstein. Hauptberuflich arbeite ich bei <a href="/#trinext">TriNext</a> als Entwickler und nebenbei bin ich als <a href="/#self-employed">Freelancer</a> aktiv.'
 
 export const navigation: NavigationItem[] = [
     { title: 'Home', url: '/' },
