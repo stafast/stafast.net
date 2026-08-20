5674852ffc1ec790f407d6a70e3fe04eeb4c46e5
stafast <stafast@outlook.com>
2026-08-20 16:55:14 +0200

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,29 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import stafast from "./stafast.config";
 
+import expressiveCode from "astro-expressive-code";
+
 export default defineConfig({
     prefetch: {
         defaultStrategy: "viewport",
     },
+
     site: stafast.site.url,
     base: stafast.site.base,
     trailingSlash: "always",
+
     vite: {
         plugins: [tailwindcss()],
     },
+
     output: "static",
     compressHTML: true,
+    integrations: [
+        expressiveCode({
+            themes: ["github-dark", "github-light"],
+            styleOverrides: {
+                codeFontSize: "0.9rem",
+            },
+        }),
+    ],
 });
